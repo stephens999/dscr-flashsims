a5b7f7168f535524c2c7768faaf3091ae3b14803
stephens999 <stephens999@gmail.com>
2018-01-21 21:00:32 -0600

    extend PMD CV; add ssvd method as well as theory; add non-sparse sims

--- code/run_dsc.R
+++ code/run_dsc.R
@@ -4,7 +4,7 @@ sessionInfo()
 
 dsc_flashsim=new_dsc("dsc-flashsim","../output/dsc-flashsim-files")
 
-sim_r1_paper =function(n=200,p=300,pi0=0.9,s=c(0.25,.5,1,2,4),tau=NULL,missing=FALSE){
+sim_r1_paper =function(n=200,p=300,pi0=0.9,s=sqrt(c(0.25,.5,1,2,4)),tau=NULL,missing=FALSE){
 
   #set tau as in paper for defaults
   if(is.null(tau)){
@@ -47,8 +47,14 @@ flash_r1_pn.wrapper = function(input,args){
   f=flashr::flash_r1(input$Y,ebnm_fn=ebnm_pn)
   return(list(LFhat = flash_get_lf(f)))
 }
+
 ssvd.wrapper = function(input,args){
-  res = ssvd::ssvd(input$Y)
+  res = ssvd::ssvd(input$Y,method = "theory")
+  return(list(LFhat = res$d * res$u %*% t(res$v)))
+}
+
+ssvd.method.wrapper = function(input,args){
+  res = ssvd::ssvd(input$Y,method = "method")
   return(list(LFhat = res$d * res$u %*% t(res$v)))
 }
 
@@ -63,7 +69,7 @@ pmd.wrapper = function(input,args){
 }
 
 pmd.cv.wrapper = function(input,args){
-  cv.out <- PMA::PMD.cv(input$Y, type="standard", sumabss=seq(0.1, 0.6, len=20),center=FALSE)
+  cv.out <- PMA::PMD.cv(input$Y, type="standard", sumabss=seq(0.1, 1, len=20),center=FALSE)
   res <-  PMA::PMD(input$Y, type="standard", sumabs=cv.out$bestsumabs, K=1, v=cv.out$v.init,center=FALSE)
   return(list(LFhat = res$d[1] * res$u[,1] %*% t(res$v[,1])))
 }
@@ -76,11 +82,13 @@ rrmse.wrapper = function(data,output){
 
 add_scenario(dsc_flashsim,"strong-sparsity-r1",sim_r1_paper.wrapper,args=list(),seed=1:20)
 add_scenario(dsc_flashsim,"intermediate-sparsity-r1",sim_r1_paper.wrapper,args=list(pi0=0.3),seed=1:20)
+add_scenario(dsc_flashsim,"no-sparsity-r1",sim_r1_paper.wrapper,args=list(pi0=0,tau=1/16),seed=1:20)
 
 
 add_method(dsc_flashsim,"flash_r1",flash_r1.wrapper)
 add_method(dsc_flashsim,"flash_r1_pn",flash_r1_pn.wrapper)
 add_method(dsc_flashsim,"ssvd",ssvd.wrapper)
+add_method(dsc_flashsim,"ssvd.method",ssvd.method.wrapper)
 add_method(dsc_flashsim,"svd",svd.wrapper)
 add_method(dsc_flashsim,"pmd",pmd.wrapper)
 add_method(dsc_flashsim,"pmd.cv",pmd.cv.wrapper)
